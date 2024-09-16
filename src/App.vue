fe3d50af9cb6e3f6855903912eb05afd05f519e9
Abdullah Özdemir <abdullahrize53@gmail.com>
2024-09-16 13:12:38 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div>
     <router-view></router-view> <!-- This renders the current route component -->
     <!-- Display logos here -->
-    <div v-if="logos.length">
+    <img class="w-full" style="filter: brightness(0.5);" src="https://cms-tabii-public-image.tabii.com/int/webp/w1600/q85/37964.jpeg">    <div v-if="logos.length" class="flex flex-row justify-between w-full fixed top-0">
       <div v-for="logo in logos" :key="logo.id" class="py-8 px-24">
         <img :src="logo.imageUrl" :alt="logo.name" class="w-32" />
+      </div>
+      <div class="flex flex-row gap-2 py-8 px-24">
+        <button class="flex justify-center items-center h-12 w-24 text-white font-bold text-xl">Türkçe</button>
+        <button class="flex justify-center items-center h-12 w-36 text-white rounded font-bold text-xl border">Giriş Yap</button>
       </div>
     </div>
     <div v-else>
